512b348193a612e97b145cb46c44d16717cd8274
kysh555 <sawakuro423@gmail.com>
2020-12-01 11:40:41 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,21 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    validates :item_price, format: { with: /[3-9][\d]{2,}|[1-9][\d]{3,6}/ }
+  end
+  with_options numericality: { other_than : 1 } do
+    validates :item_category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+  end
+
   belongs_to :user
   has_one :purchase
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_category
   belongs_to_active_hash :item_status
