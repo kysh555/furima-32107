190e63dbe9a3440af35068f7a7080c00da8061bb
kysh555 <sawakuro423@gmail.com>
2020-12-01 14:42:16 +0900

    出品商品の情報をテーブルに保存できるよう設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
     validates :item_description
     validates :item_price, format: { with: /[3-9][\d]{2,}|[1-9][\d]{3,6}/ }
   end
-  with_options numericality: { other_than : 1 } do
+  with_options numericality: { other_than: 1 } do
     validates :item_category_id
     validates :item_status_id
     validates :shipping_cost_id
@@ -15,11 +15,13 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one :purchase
-  
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_category
   belongs_to_active_hash :item_status
   belongs_to_active_hash :shipping_cost
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :days_to_ship
+
 end
