67ba9e210c95e713e9508175cc28d95f6096a772
kysh555 <sawakuro423@gmail.com>
2020-12-02 15:14:46 +0900

    item_priceバリデーション変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,8 @@ class Item < ApplicationRecord
     validates :image
     validates :item_name
     validates :item_description
-    validates :item_price, format: { with: /[3-9][\d]{2,}|[1-9][\d]{3,6}/ }
-    with_options numericality: { other_than: 1 } do
+    validates :item_price, numericality:  { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
+    with_options numericality: { other_than: 1, message: "is not selected" } do
       validates :item_category_id
       validates :item_status_id
       validates :shipping_cost_id
