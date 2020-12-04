0f3e0a19620b2b4839d2372e41c3eaf14664401d
kysh555 <sawakuro423@gmail.com>
2020-12-04 17:53:30 +0900

    Formオブジェクトにデータが保存できるよう記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,4 +5,21 @@ class PurchasesController < ApplicationController
     @purchase_address = PurchaseAddress.new
   end
 
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_address_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to '/'
+    else
+      @item = Item.find(params[:item_id])
+      render 'purchases/index'
+    end
+  end
+
+  private
+
+  def purchase_address_params
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id).merge(user_id: current_user.id)
+  end
+
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,20 +1,18 @@
 class PurchaseAddress 
   include ActiveModel::Model
-  attr_accessor :item_id, :use_id, :post_code, :prefecture_id, :city, :addrss, :building, :phone_number
+  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :address, :building, :phone_number
 
   with_options presence: true do
-    validates :post_code
-    validates :prefecture_id
+    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/, message:'is invalid' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'is not selected' }
     validates :city
-    validates :addrss
-    validates :phone_number
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }, format: { with: /\A\d{,11}\z/, message: 'is invalid' }
   end
 
   def save
-    item = Item.find(params[:id])
-    user = item.user
-    Purchase.create(item_id: item.id, user_id: user.id)
-    ShipToAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, addrss: addrss, building: building, phone_number: phone_number)
+    Purchase.create(item_id: item_id, user_id: user_id)
+    ShipToAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number)
   end
 
 end
