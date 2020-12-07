529f8d6116a4ac951898b834b13c7fbb2b669d85
kysh555 <sawakuro423@gmail.com>
2020-12-07 12:29:20 +0900

    mergeメソッドをひとまとめに修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -32,7 +32,7 @@ class PurchasesController < ApplicationController
   end
 
   def purchase_address_params
-    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id]).merge(user_id: current_user.id).merge(token: params[:token])
+    params.require(:purchase_address).permit(:post_code, :prefecture_id, :city, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def move_to_index
