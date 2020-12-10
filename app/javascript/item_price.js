6f9eaed64c62151b2809112b5597f182700dcad3
kysh555 <sawakuro423@gmail.com>
2020-12-10 12:12:27 +0900

    JSファイルの対象を限定する記述を追加

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,14 @@
-window.addEventListener('load', () => {
-  const priceInput = document.getElementById("item-price");
-  const addTaxPrice = document.getElementById("add-tax-price");
-  const profit = document.getElementById("profit");
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  window.addEventListener('load', () => {
+    const priceInput = document.getElementById("item-price");
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
 
-  priceInput.addEventListener('input', () => {
-    const inputValue = priceInput.value
-    const tax = Math.floor(inputValue * 0.1);
-    addTaxPrice.innerHTML = tax;
-    profit.innerHTML = inputValue - tax;
+    priceInput.addEventListener('input', () => {
+      const inputValue = priceInput.value
+      const tax = Math.floor(inputValue * 0.1);
+      addTaxPrice.innerHTML = tax;
+      profit.innerHTML = inputValue - tax;
+    });
   });
-});
+}
